2930f13d38dbeb81616233b79a38feb42024bf0c
Bruno8310 <gongl3730@gmail.com>
2020-08-26 17:44:54 +0800

    [mocking][Bruno/Jessie] feature: implement testPark_givenAVipCarAndAFullParkingLog_thenGiveAReceiptWithCarNameAndParkingLotName

--- src/test/java/parking/VipParkingStrategyTest.java
+++ src/test/java/parking/VipParkingStrategyTest.java
@@ -2,6 +2,9 @@ package parking;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 
 public class VipParkingStrategyTest {
@@ -11,6 +14,22 @@ public class VipParkingStrategyTest {
 
 	    /* Exercise 4, Write a test case on VipParkingStrategy.park()
 	    * With using Mockito spy, verify and doReturn */
+        // given
+        VipParkingStrategy vipParkingStrategy = spy(new VipParkingStrategy());
+
+        Car car = this.createMockCar("A");
+        doReturn(true).when(vipParkingStrategy).isAllowOverPark(car);
+
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLot = mock(ParkingLot.class);
+
+        when(parkingLot.isFull()).thenReturn(true);
+        parkingLots.add(parkingLot);
+        // when
+        vipParkingStrategy.park(parkingLots, car);
+
+        // then
+        verify(vipParkingStrategy).createReceipt(parkingLot ,car);
 
     }
 
